b32a09fcdc50964a50c37597498b8e1c2ac74e47
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-06-27 16:04:09 -0500

    up

--- W1D5/tiltejs.js
+++ W1D5/tiltejs.js
@@ -47,7 +47,7 @@ console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(4,
     
     }
     console.log("Expectid output of isvowel(a) is true  "  + myFunctionTest(true, isVowel("a") ));
-  
+ 
 
 // <!-- 4.Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. 
 // For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24. -->
